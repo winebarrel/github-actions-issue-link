ab668d5a600bed6b495e707b4cc024b9361b1010
winebarrel <sugawara@winebarrel.jp>
2023-03-11 17:41:33 +0900

    Fix link making

--- background.js
+++ background.js
@@ -1,17 +1,22 @@
-function replaceIssueLink() {
+function replaceIssueLink(url) {
   const span = document.querySelector("h3.PageHeader-title > span > span");
 
-  if (span) {
-    const text = span.textContent;
-    const m = text.match(/ #(\d+) /);
-
-    if (m) {
-      const num = m[1];
-      const link = `<a href="../../issues/${num}">#${num}</a>`;
-      const textWithLink = text.replace(/#\d+/, link);
-      span.innerHTML = textWithLink;
-    }
+  if (!span) {
+    return;
   }
+
+  const text = span.textContent;
+  const m = text.match(/ #(\d+) /);
+
+  if (!m) {
+    return;
+  }
+
+  const num = m[1];
+  const baseURL = url.split("/").slice(0, 5).join("/");
+  const link = `<a href="${baseURL}/issues/${num}">#${num}</a>`;
+  const textWithLink = text.replace(/#\d+/, link);
+  span.innerHTML = textWithLink;
 }
 
 chrome.tabs.onUpdated.addListener(async (tabId, props, tab) => {
@@ -22,6 +27,7 @@ chrome.tabs.onUpdated.addListener(async (tabId, props, tab) => {
     chrome.scripting.executeScript({
       target: { tabId },
       function: replaceIssueLink,
+      args: [tab.url],
     });
   }
 });
